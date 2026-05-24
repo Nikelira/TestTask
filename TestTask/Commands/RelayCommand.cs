deb3bcfb0309cd64b72d92c4712bf349dfb74aec
Афанасьева Алиса <avafanaseva@phtt.ru>
2026-05-24 23:38:34 +0500

    Классы для создания команд

---
+++ TestTask/Commands/RelayCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using TestTask.Commands.Base;
+
+namespace TestTask.Commands
+{
+    internal class RelayCommand : Command
+    {
+        private readonly Action<object> _Execute;
+        private readonly Func<object, bool> _CanExecute;
+        public RelayCommand(Action<object> Execute, Func<object, bool> CanExecute = null)
+        {
+            _Execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            _CanExecute = CanExecute;
+        }
+
+        public override bool CanExecute(object parameter) => _CanExecute?.Invoke(parameter) ?? true;
+
+        public override void Execute(object parameter)
+        {
+            _Execute?.Invoke(parameter);
+        }
+    }
+}
