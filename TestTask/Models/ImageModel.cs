3f99a5ac7815c72c3c2240415fd3a93fd3a14720
Афанасьева Алиса <avafanaseva@phtt.ru>
2026-05-24 21:44:51 +0500

    Добавлена модель для работы с картинками

---
+++ TestTask/Models/ImageModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace TestTask.Models
+{
+    internal class ImageModel : INotifyPropertyChanged
+    {
+        private string _url;
+        private BitmapImage _image;
+        private bool _isLoad;
+        private int _progress;
+
+        public string Url
+        {
+            get { return _url; }
+            set
+            {
+                _url = value;
+                OnPropertyChanged("Url");
+            }
+        }
+        public BitmapImage BitmapImage
+        {
+            get { return _image; }
+            set
+            {
+                _image = value;
+                OnPropertyChanged("Image");
+            }
+        }
+        public bool IsLoad
+        {
+            get { return _isLoad; }
+            set
+            {
+                _isLoad = value;
+                OnPropertyChanged("IsLoad");
+            }
+        }
+        public int Progress
+        {
+            get { return _progress; }
+            set
+            {
+                _progress = value;
+                OnPropertyChanged("Progress");
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
